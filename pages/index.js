e96459f8dadffab6cf3e1838c20cfa38d80f9cb7
Pilchness <44652038+Pilchness@users.noreply.github.com>
2020-11-29 11:15:09 +0000

    numbersInit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,80 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const colours = [
+  'blue',
+  'green',
+  'red',
+  'yellow',
+  'red',
+  'yellow',
+  'blue',
+  'green',
+  'red',
+  'yellow',
+  'yellow',
+  'red',
+  'yellow',
+  'blue',
+  'green',
+  'red'
+];
+
+const contents = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 export default function Home() {
+  const gridBlocks = [];
+  for (let i = 1; i < 5; i++) {
+    for (let j = 1; j < 5; j++) {
+      let counter = j + 4 * (i - 1);
+      gridBlocks.push(
+        <div
+          key={'block' + counter}
+          id={'block' + counter}
+          onClick={() => {
+            document.getElementById('block' + counter).style.backgroundColor = colours[counter - 1];
+            document.getElementById('block' + counter).style.color = 'black';
+          }}
+          onMouseEnter={() => {
+            document.getElementById('block' + counter).style.backgroundColor = 'black';
+            document.getElementById('block' + counter).style.color = 'white';
+
+            document.getElementById('block' + counter).style.transition = 'all 2s ease-in-out';
+          }}
+          onMouseLeave={() => {
+            //document.getElementById('block' + counter).style.backgroundColor = colours[counter - 1];
+            document.getElementById('block' + counter).style.color = 'black';
+          }}
+          style={{
+            gridColumn: j,
+            gridRow: i,
+            backgroundColor: colours[counter - 1],
+            margin: '2px',
+            textAlign: 'center',
+            fontSize: '800%',
+            fontFamily: 'sans-serif',
+            display: 'grid',
+            placeItems: 'center',
+            cursor: 'pointer'
+          }}
+        >
+          {contents[counter - 1]}
+        </div>
+      );
+    }
+  }
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Numbers</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {gridBlocks.map((block) => {
+          return block;
+        })}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
